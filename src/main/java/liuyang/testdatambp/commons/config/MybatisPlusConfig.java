393eadcafd3c5399a7a045f5a4d609bc33ffac4f
liuyang <xconfigurator@hotmail.com>
2022-07-15 12:17:09 +0800

    添加sql性能规范插件。

--- src/main/java/liuyang/testdatambp/commons/config/MybatisPlusConfig.java
+++ src/main/java/liuyang/testdatambp/commons/config/MybatisPlusConfig.java
@@ -2,12 +2,15 @@ package liuyang.testdatambp.commons.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.IllegalSQLInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import net.bytebuddy.implementation.bind.annotation.FieldProxy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * @author liuyang(wx)
@@ -36,12 +39,15 @@ public class MybatisPlusConfig {
         // 乐观锁插件（不需要额外加pom.xml依赖） + @Version （如果冲突，第二条不会被执行，可以通过更新语句返回的影响条数来进行判断）
         // doc: https://baomidou.com/pages/0d93c0/
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        // sql性能规范
+        interceptor.addInnerInterceptor(new IllegalSQLInnerInterceptor());
         return interceptor;
     }
 
-    // 性能分析插件，开发调试环境使用
+    // 性能分析插件，开发调试环境使用(新版本已改为“sql性能规范 IllegalSQLInnerInterceptor”)
     /*
-    //@Bean
+    @Bean
+    @Profile({"dev", "test"})
     public PerformanceInterceptor performanceInterceptor() {
         PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
         // maxTime 指的是 sql 最大执行时长
